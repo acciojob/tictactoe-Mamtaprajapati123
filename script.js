5e24cc84f36e61f6442044639ac9a8b08ad15726
Mamtaprajapati123 <124058185+Mamtaprajapati123@users.noreply.github.com>
2023-04-15 00:33:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your JS code here. If required.
+const form = document.getElementById('form');
+const message = document.querySelector('.message');
+const cells = document.querySelectorAll('.cell');
+let player1 = '';
+let player2 = '';
+let currentPlayer = 'x';
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  player1 = document.getElementById('player-1').value;
+  player2 = document.getElementById('player-2').value;
+  message.textContent = `${player1}, you're up!`;
+  form.style.display = 'none';
+});
+
+cells.forEach((
+
